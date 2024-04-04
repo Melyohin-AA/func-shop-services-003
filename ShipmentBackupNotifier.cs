23546daac8b7ae49e34689b8f0b986a608198acb
thalber <filefillman@gmail.com>
2024-04-04 17:51:28 +0400

    forgot to add early return in notifier

--- ShipmentBackupNotifier.cs
+++ ShipmentBackupNotifier.cs
@@ -11,9 +11,11 @@ internal class ShipmentBackupNotifier
 	public ShipmentBackupNotifier(ILogger logger, EmailSender email)
 	{
 		this.logger = logger;
-		if (addressToSendNotificationsTo is null or "") {
+		if (addressToSendNotificationsTo is null or "")
+		{
 			logger.LogError($"SHOPSERVICES_BACKUPS_MAIL must be set for email notifications to work.");
 			logger.LogError($"Notifications will fail.");
+			return;
 		}
 		this.email = email;
 	}
@@ -21,7 +23,10 @@ internal class ShipmentBackupNotifier
 		NotificationReason notificationReason,
 		ShopServices.Shop.Storing.Models.Shipment shipment)
 	{
-
+		if (email is null)
+		{
+			logger.LogError($"Cannot send notification for {notificationReason}, {shipment.Id}: Notifier not initialized");
+		}
 		EmailTextBuilder textBuilder = new();
 		textBuilder
 			.AppendLine($"This is a notification for: {notificationReason}")
